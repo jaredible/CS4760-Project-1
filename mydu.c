5ac0cd788d6ed3b9a2c20cae8e17faecdbd82a52
Jared Diehl <diehl@hoare7.cs.umsl.edu>
2020-09-12 08:06:26 -0500

    Initial implementation of functions

--- mydu.c
+++ mydu.c
@@ -1,9 +1,17 @@
 #include <stdlib.h>
 
-int depthfirstapply(char *path, int pathfun(char *path1));
+int depthfirstapply(char *path, int pathfun(char *path1), int depth);
 int sizepathfun(char *path);
 
 int main(int argc, char *argv[])
 {
 	return EXIT_SUCCESS;
+}
+
+int depthfirstapply(char *path, int pathfun(char *path1), int depth) {
+	return 0;
+}
+
+int sizepathfun(char *path) {
+	return 0;
 }
