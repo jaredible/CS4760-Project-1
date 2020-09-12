fbc4f9db7443c85b540f447feab502e29113e080
Jared Diehl <diehl@hoare7.cs.umsl.edu>
2020-09-12 08:15:56 -0500

    Implemented getting options

--- mydu.c
+++ mydu.c
@@ -1,10 +1,38 @@
+#include <getopt.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+char options[10];
+int scaler = -1;
+int max_depth = -1;
 
 int depthfirstapply(char *path, int pathfun(char *path1), int depth);
 int sizepathfun(char *path);
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+	int opt;
+	
+	while ((opt = getopt(argc, argv, "haB:bmcd:HLs")) != -1) {
+		char str[2];
+		sprintf(str, "%c", opt);
+		strcat(options, str);
+		
+		switch (opt) {
+			case 'B':
+				scaler = atoi(optarg);
+				break;
+			case 'd':
+				max_depth = atoi(optarg);
+				break;
+			case 'm':
+				scaler = 1024 * 1024;
+				break;
+			default:
+				break;
+		}
+	}
+	
 	return EXIT_SUCCESS;
 }
 
